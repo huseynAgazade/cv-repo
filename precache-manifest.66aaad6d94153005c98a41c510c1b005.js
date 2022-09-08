47a3122c4abee87045d21ba8a1ac52c709a6d29e
huseynAgazade <huseynagazade969@gmail.com>
2022-09-08 22:31:46 +0400

    Updates

---
+++ precache-manifest.66aaad6d94153005c98a41c510c1b005.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b87675ca09901fd90563150dc8eb0f",
+    "url": "/cv-repo/index.html"
+  },
+  {
+    "revision": "3710aacfb7f925f5e43d",
+    "url": "/cv-repo/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "cc8a392b13bd0075eb17",
+    "url": "/cv-repo/static/css/main.f1dfb855.chunk.css"
+  },
+  {
+    "revision": "3710aacfb7f925f5e43d",
+    "url": "/cv-repo/static/js/2.7659ed11.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/cv-repo/static/js/2.7659ed11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8a392b13bd0075eb17",
+    "url": "/cv-repo/static/js/main.2a9f35e2.chunk.js"
+  },
+  {
+    "revision": "51088c863762efe3cd88",
+    "url": "/cv-repo/static/js/runtime-main.654d56fe.js"
+  },
+  {
+    "revision": "79c8c45f4eda01ec83d7533b231e751c",
+    "url": "/cv-repo/static/media/CSessentials.79c8c45f.pdf"
+  },
+  {
+    "revision": "78401eca466a460d056c4f36df9486e7",
+    "url": "/cv-repo/static/media/NDGLinux.78401eca.pdf"
+  },
+  {
+    "revision": "78a04fb98ef2b82524906117ff20d7a3",
+    "url": "/cv-repo/static/media/Person1.78a04fb9.jpg"
+  },
+  {
+    "revision": "0d0c2fb651b8dee22bc3f169c682e383",
+    "url": "/cv-repo/static/media/introCS.0d0c2fb6.pdf"
+  }
+]);
